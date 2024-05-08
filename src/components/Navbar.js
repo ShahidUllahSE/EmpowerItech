49327075c2ad626408f8486adbeff97e9c3d40ee
ShahidUllahSE <shahidbangash8899@gmail.com>
2024-05-08 13:25:33 +0500

    pull

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -71,7 +71,7 @@ const Navbar = () => {
 
         
 
-        {/* Navigation links for larger screenss */}
+        {/* Navigation links for larger screens*/}
         <div className="hidden md:flex space-x-9 justify-items-center mr-80">
           {/* Links */}
           <a href="/" className="hover:underline hover:underline-offset-8 active:bg-pink-600 hover:text-pink-500 cursor-pointer font-normal">Home</a>
